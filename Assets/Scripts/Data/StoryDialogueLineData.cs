f8d90a2f6e80b720c94554645ecb8639114f656b
DoveGu-gu <2309338586@qq.com>
2026-07-26 23:15:52 +0800

    fix：case_1002资产修复

--- Assets/Scripts/Data/StoryDialogueLineData.cs
+++ Assets/Scripts/Data/StoryDialogueLineData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [Serializable]
 public class StoryDialogueLineData
 {
-    [TextArea(2, 6)]
+    [TextArea(2, 10)]
     public string text;
     public bool showPortrait;
     public Sprite portrait;
